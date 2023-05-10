67c1905e23f04819ae2f31af6324e05271788237
Revi-Universe <dldnwn99@gmail.com>
2023-05-10 16:18:25 +0900

    헤더와 기본적인 페이지 라우트

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<style>
+    div {
+        max-width: 1024px;
+        margin: 0 auto;
+    }
+
+    header {
+        width: 100%;
+        height: 60px;
+        background-color: #484848;
+        display: flex;
+    }
+
+    header a {
+        display: inline-block;
+        height: 100%;
+        margin-left: 16px;
+        color: #f2f2f2;
+        display: flex;
+        align-items: center;
+        text-decoration-line: none;
+    }
+    header a:hover {
+        color: #b4b4b4;
+    }
+    header a:first-child {
+        margin: 0 24px 0 32px;
+        font-size: 20pt;
+        font-weight: bold;
+    }
+</style>
+
+<header>
+    <a href="/">Slime Cave</a>
+    <a href="/find">알 찾기</a>
+</header>
+<div>
+    <slot />
+</div>
---
+++ src/routes/find/+page.svelte
@@ -0,0 +1,2 @@
+<a href="/find/forest">숲</a>
+<a href="/find/ocean">바다</a>
